553854e202a28a9f1a282dfe9943f7f8736465a0
Reynaldo Morillo <reymorillo2@gmail.com>
2021-11-10 19:47:04 -0500

    Added another superprefix attack.

---
+++ lib_rovpp/tests/yaml_system_tests/superprefix_prefix_graph_11/test_superprefix_prefix_02.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from lib_bgp_simulator import BaseGraphSystemTester, BGPSimpleAS, ROVSimpleAS, Graph011
+
+from ....as_classes import ROVPPV1SimpleAS
+from ....as_classes import ROVPPV2SimpleAS
+from ....as_classes import ROVPPV2aSimpleAS
+from ....as_classes import ROVPPV3AS
+
+from ....engine_input import ROVPPSuperprefixPrefixHijack
+
+
+class BaseSuperPrefixPrefix02Tester(BaseGraphSystemTester):
+    GraphInfoCls = Graph011
+    BaseASCls = BGPSimpleAS
+    EngineInputCls = ROVPPSuperprefixPrefixHijack
+    base_dir = Path(__file__).parent
+    adopting_asns = (5, 6, 1, 11, 12)
+
+
+class Test020SupreprefixPrefix02(BaseSuperPrefixPrefix02Tester):
+    AdoptASCls = ROVSimpleAS
+
+class Test021SupreprefixPrefix02(BaseSuperPrefixPrefix02Tester):
+    AdoptASCls = ROVPPV1SimpleAS
+
+class Test022SupreprefixPrefix02(BaseSuperPrefixPrefix02Tester):
+    AdoptASCls = ROVPPV2SimpleAS
+
+class Test023SupreprefixPrefix02(BaseSuperPrefixPrefix02Tester):
+    AdoptASCls = ROVPPV2aSimpleAS
+
