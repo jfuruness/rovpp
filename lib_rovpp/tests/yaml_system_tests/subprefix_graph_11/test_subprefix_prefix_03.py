ff19409e3e1806348a90f78a78e4edb362946273
Reynaldo Morillo <reymorillo2@gmail.com>
2021-11-19 17:35:16 -0500

    Added more tests for subprefix hijacking and it seems that we may have a bug in subprefix_graph_11.

---
+++ lib_rovpp/tests/yaml_system_tests/subprefix_graph_11/test_subprefix_prefix_03.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from lib_bgp_simulator import BaseGraphSystemTester, BGPSimpleAS, ROVSimpleAS, Graph011
+
+from ....as_classes import ROVPPV1SimpleAS
+from ....as_classes import ROVPPV2SimpleAS
+from ....as_classes import ROVPPV2aSimpleAS
+
+from ....engine_input import ROVPPSubprefixHijack
+
+
+class BaseSubPrefix01Tester(BaseGraphSystemTester):
+    GraphInfoCls = Graph011
+    BaseASCls = BGPSimpleAS
+    EngineInputCls = ROVPPSubprefixHijack
+    base_dir = Path(__file__).parent
+    adopting_asns = (5, 6, 1, 11, 12)
+
+
+class Test052SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVSimpleAS
+
+class Test053SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVPPV1SimpleAS
+
+class Test054SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVPPV2SimpleAS
+
+class Test055SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVPPV2aSimpleAS
+
---
+++ lib_rovpp/tests/yaml_system_tests/subprefix_graph_13/test_subprefix_prefix_01.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from lib_bgp_simulator import BaseGraphSystemTester, BGPSimpleAS, ROVSimpleAS, Graph013
+
+from ....as_classes import ROVPPV1SimpleAS
+from ....as_classes import ROVPPV2SimpleAS
+from ....as_classes import ROVPPV2aSimpleAS
+
+from ....engine_input import ROVPPSubprefixHijack
+
+
+class BaseSubPrefix01Tester(BaseGraphSystemTester):
+    GraphInfoCls = Graph013
+    BaseASCls = BGPSimpleAS
+    EngineInputCls = ROVPPSubprefixHijack
+    base_dir = Path(__file__).parent
+    adopting_asns = (2, )
+
+
+class Test052SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVSimpleAS
+
+class Test053SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVPPV1SimpleAS
+
+class Test054SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVPPV2SimpleAS
+
+class Test055SubprefixPrefix01(BaseSubPrefix01Tester):
+    AdoptASCls = ROVPPV2aSimpleAS
+
