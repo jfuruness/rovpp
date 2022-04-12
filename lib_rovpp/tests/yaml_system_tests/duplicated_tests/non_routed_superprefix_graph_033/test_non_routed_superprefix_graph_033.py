d8386dc704297da510735e7dc7b567b9d5847ac5
Reynaldo Morillo <reymorillo2@gmail.com>
2022-04-12 13:45:32 -0400

    Added 2 tests each for type 2 and 3  (prefix-hijack attack on a non-routed prefix  and   superprefix attack on a non-routed prefix  respectively).

---
+++ lib_rovpp/tests/yaml_system_tests/duplicated_tests/non_routed_superprefix_graph_033/test_non_routed_superprefix_graph_033.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from lib_bgp_simulator import BaseGraphSystemTester, BGPSimpleAS, Graph033
+from lib_bgp_simulator import enums
+
+from ...unstable import Unstable
+from .....as_classes import ROVPPV1SimpleAS
+from .....as_classes import ROVPPV2SimpleAS
+from .....as_classes import ROVPPV2aSimpleAS
+
+from .....engine_input import ROVPPNonRoutedSuperprefixHijack
+
+
+class BaseSubprefixGraph033Tester(Unstable, BaseGraphSystemTester):
+    GraphInfoCls = Graph033
+    BaseASCls = BGPSimpleAS
+    EngineInputCls = ROVPPNonRoutedSuperprefixHijack
+    base_dir = Path(__file__).parent
+    adopting_asns = (3, 4, 6, enums.ASNs.VICTIM.value, )  # Do I need to add the victim here?
+
+
+class Test108(BaseSubprefixGraph033Tester):
+    AdoptASCls = ROVPPV1SimpleAS
+
+
+class Test109(BaseSubprefixGraph033Tester):
+    AdoptASCls = ROVPPV2SimpleAS
+
+
+class Test110(BaseSubprefixGraph033Tester):
+    AdoptASCls = ROVPPV2aSimpleAS
+
