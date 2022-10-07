9d0163c6e8b3d15cdfa9ad13236d8aef4659f0b5
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2022-10-07 17:26:50 -0400

    Added all the test numbers we will use for system tests
    When creating a new test from now on, just uncomment the config that was created. This will help avoid merge conflicts later.

--- rovpp_pkg/tests/engine_tests/engine_test_configs/__init__.py
+++ rovpp_pkg/tests/engine_tests/engine_test_configs/__init__.py
@@ -74,92 +74,298 @@ from .config_107 import Config107
 from .config_108 import Config108
 from .config_109 import Config109
 from .config_110 import Config110
+# from .config_111 import Config111
+# from .config_112 import Config112
+# from .config_113 import Config113
+# from .config_114 import Config114
+# from .config_115 import Config115
+# from .config_116 import Config116
+# from .config_117 import Config117
+# from .config_118 import Config118
+# from .config_119 import Config119
+# from .config_120 import Config120
+# from .config_121 import Config121
+# from .config_122 import Config122
+# from .config_123 import Config123
+# from .config_124 import Config124
+# from .config_125 import Config125
+# from .config_126 import Config126
+# from .config_127 import Config127
+# from .config_128 import Config128
+# from .config_129 import Config129
+# from .config_130 import Config130
+# from .config_131 import Config131
+# from .config_132 import Config132
+# from .config_133 import Config133
+# from .config_134 import Config134
+# from .config_135 import Config135
+# from .config_136 import Config136
+# from .config_137 import Config137
+# from .config_138 import Config138
+# from .config_139 import Config139
+# from .config_140 import Config140
+# from .config_141 import Config141
+# from .config_142 import Config142
+# from .config_143 import Config143
+# from .config_144 import Config144
+# from .config_145 import Config145
+# from .config_146 import Config146
+# from .config_147 import Config147
+# from .config_148 import Config148
+# from .config_149 import Config149
+# from .config_150 import Config150
+# from .config_151 import Config151
+# from .config_152 import Config152
+# from .config_153 import Config153
+# from .config_154 import Config154
+# from .config_155 import Config155
+# from .config_156 import Config156
+# from .config_157 import Config157
+# from .config_158 import Config158
+# from .config_159 import Config159
+# from .config_160 import Config160
+# from .config_161 import Config161
+# from .config_162 import Config162
+# from .config_163 import Config163
+# from .config_164 import Config164
+# from .config_165 import Config165
+# from .config_166 import Config166
+# from .config_167 import Config167
+# from .config_168 import Config168
+# from .config_169 import Config169
+# from .config_170 import Config170
+# from .config_171 import Config171
+# from .config_172 import Config172
+# from .config_173 import Config173
+# from .config_174 import Config174
+# from .config_175 import Config175
+# from .config_176 import Config176
+# from .config_177 import Config177
+# from .config_178 import Config178
+# from .config_179 import Config179
+# from .config_180 import Config180
+# from .config_181 import Config181
+# from .config_182 import Config182
+# from .config_183 import Config183
+# from .config_184 import Config184
+# from .config_185 import Config185
+# from .config_186 import Config186
+# from .config_187 import Config187
+# from .config_188 import Config188
+# from .config_189 import Config189
+# from .config_190 import Config190
+# from .config_191 import Config191
+# from .config_192 import Config192
+# from .config_193 import Config193
+# from .config_194 import Config194
+# from .config_195 import Config195
+# from .config_196 import Config196
+# from .config_197 import Config197
+# from .config_198 import Config198
+# from .config_199 import Config199
+# from .config_200 import Config200
+# from .config_201 import Config201
+# from .config_202 import Config202
+# from .config_203 import Config203
+# from .config_204 import Config204
+# from .config_205 import Config205
+# from .config_206 import Config206
+# from .config_207 import Config207
+# from .config_208 import Config208
+# from .config_209 import Config209
+# from .config_210 import Config210
+# from .config_211 import Config211
+# from .config_212 import Config212
+# from .config_213 import Config213
+# from .config_214 import Config214
 from .config_215 import Config215
 from .config_216 import Config216
 from .config_217 import Config217
 from .config_218 import Config218
 
-
-
-
-__all__ = ["Config035",
-           "Config036",
-           "Config037",
-           "Config038",
-           "Config039",
-           "Config040",
-           "Config041",
-           "Config042",
-           "Config043",
-           "Config044",
-           "Config045",
-           "Config046",
-           "Config047",
-           "Config048",
-           "Config049",
-           "Config050",
-           "Config051",
-           "Config052",
-           "Config053",
-           "Config054",
-           "Config055",
-           "Config056",
-           "Config057",
-           "Config058",
-           "Config059",
-           "Config060",
-           "Config061",
-           "Config062",
-           "Config063",
-           "Config064",
-           "Config065",
-           "Config066",
-           "Config067",
-           "Config068",
-           "Config069",
-           "Config070",
-           "Config071",
-           "Config072",
-           "Config073",
-           "Config074",
-           "Config075",
-           "Config076",
-           "Config077",
-           "Config078",
-           "Config079",
-           "Config080",
-           "Config081",
-           "Config082",
-           "Config083",
-           "Config084",
-           "Config085",
-           "Config086",
-           "Config087",
-           "Config088",
-           "Config089",
-           "Config090",
-           "Config091",
-           "Config092",
-           "Config093",
-           "Config094",
-           "Config095",
-           "Config096",
-           "Config097",
-           "Config098",
-           "Config099",
-           "Config100",
-           "Config101",
-           "Config102",
-           "Config103",
-           "Config104",
-           "Config105",
-           "Config106",
-           "Config107",
-           "Config108",
-           "Config109",
-           "Config110",
-           "Config215",
-           "Config216",
-           "Config217",
-           "Config218"
-           ]
+__all__ = [
+    "Config035",
+    "Config036",
+    "Config037",
+    "Config038",
+    "Config039",
+    "Config040",
+    "Config041",
+    "Config042",
+    "Config043",
+    "Config044",
+    "Config045",
+    "Config046",
+    "Config047",
+    "Config048",
+    "Config049",
+    "Config050",
+    "Config051",
+    "Config052",
+    "Config053",
+    "Config054",
+    "Config055",
+    "Config056",
+    "Config057",
+    "Config058",
+    "Config059",
+    "Config060",
+    "Config061",
+    "Config062",
+    "Config063",
+    "Config064",
+    "Config065",
+    "Config066",
+    "Config067",
+    "Config068",
+    "Config069",
+    "Config070",
+    "Config071",
+    "Config072",
+    "Config073",
+    "Config074",
+    "Config075",
+    "Config076",
+    "Config077",
+    "Config078",
+    "Config079",
+    "Config080",
+    "Config081",
+    "Config082",
+    "Config083",
+    "Config084",
+    "Config085",
+    "Config086",
+    "Config087",
+    "Config088",
+    "Config089",
+    "Config090",
+    "Config091",
+    "Config092",
+    "Config093",
+    "Config094",
+    "Config095",
+    "Config096",
+    "Config097",
+    "Config098",
+    "Config099",
+    "Config100",
+    "Config101",
+    "Config102",
+    "Config103",
+    "Config104",
+    "Config105",
+    "Config106",
+    "Config107",
+    "Config108",
+    "Config109",
+    "Config110",
+    # "Config111",
+    # "Config112",
+    # "Config113",
+    # "Config114",
+    # "Config115",
+    # "Config116",
+    # "Config117",
+    # "Config118",
+    # "Config119",
+    # "Config120",
+    # "Config121",
+    # "Config122",
+    # "Config123",
+    # "Config124",
+    # "Config125",
+    # "Config126",
+    # "Config127",
+    # "Config128",
+    # "Config129",
+    # "Config130",
+    # "Config131",
+    # "Config132",
+    # "Config133",
+    # "Config134",
+    # "Config135",
+    # "Config136",
+    # "Config137",
+    # "Config138",
+    # "Config139",
+    # "Config140",
+    # "Config141",
+    # "Config142",
+    # "Config143",
+    # "Config144",
+    # "Config145",
+    # "Config146",
+    # "Config147",
+    # "Config148",
+    # "Config149",
+    # "Config150",
+    # "Config151",
+    # "Config152",
+    # "Config153",
+    # "Config154",
+    # "Config155",
+    # "Config156",
+    # "Config157",
+    # "Config158",
+    # "Config159",
+    # "Config160",
+    # "Config161",
+    # "Config162",
+    # "Config163",
+    # "Config164",
+    # "Config165",
+    # "Config166",
+    # "Config167",
+    # "Config168",
+    # "Config169",
+    # "Config170",
+    # "Config171",
+    # "Config172",
+    # "Config173",
+    # "Config174",
+    # "Config175",
+    # "Config176",
+    # "Config177",
+    # "Config178",
+    # "Config179",
+    # "Config180",
+    # "Config181",
+    # "Config182",
+    # "Config183",
+    # "Config184",
+    # "Config185",
+    # "Config186",
+    # "Config187",
+    # "Config188",
+    # "Config189",
+    # "Config190",
+    # "Config191",
+    # "Config192",
+    # "Config193",
+    # "Config194",
+    # "Config195",
+    # "Config196",
+    # "Config197",
+    # "Config198",
+    # "Config199",
+    # "Config200",
+    # "Config201",
+    # "Config202",
+    # "Config203",
+    # "Config204",
+    # "Config205",
+    # "Config206",
+    # "Config207",
+    # "Config208",
+    # "Config209",
+    # "Config210",
+    # "Config211",
+    # "Config212",
+    # "Config213",
+    # "Config214",
+    "Config215",
+    "Config216",
+    "Config217",
+    "Config218"
+]
