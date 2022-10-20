0a7e566b45ef7dd0806aeb0e1faf8194e219851f
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2022-10-20 17:56:29 -0400

    Added System tests from 167 -194
    All verified to be working.
    Identified that tests 164, 165, and 166 are not correct.

---
+++ rovpp_pkg/tests/engine_tests/engine_test_configs/config_171.py
@@ -0,0 +1,34 @@
+from typing import Dict, Type
+
+from caida_collector_pkg import AS
+
+from bgp_simulator_pkg import graphs
+from bgp_simulator_pkg import EngineTestConfig
+
+from bgp_simulator_pkg import BGPSimpleAS
+from bgp_simulator_pkg import ASNs
+from bgp_simulator_pkg import ROVSimpleAS
+from bgp_simulator_pkg import SubprefixHijack
+
+from rovpp_pkg import ROVPPAnn
+
+
+class Config171(EngineTestConfig):
+    """Contains config options to run a test"""
+
+    name = "171"
+    desc = "Subprefix Hijack Attack with ROV"
+    scenario = SubprefixHijack(attacker_asns={ASNs.ATTACKER.value},
+                               victim_asns={ASNs.VICTIM.value},
+                               AdoptASCls=ROVSimpleAS,
+                               BaseASCls=BGPSimpleAS,
+                               AnnCls=ROVPPAnn)
+    graph = graphs.Graph036()
+    non_default_as_cls_dict: Dict[int, Type[AS]] = {
+        5: ROVSimpleAS,
+        8: ROVSimpleAS,
+        10: ROVSimpleAS,
+        15: ROVSimpleAS,
+        ASNs.VICTIM.value: ROVSimpleAS
+    }
+    propagation_rounds = 1
---
+++ rovpp_pkg/tests/engine_tests/engine_test_configs/config_181.py
@@ -0,0 +1,34 @@
+from typing import Dict, Type
+
+from caida_collector_pkg import AS
+
+from bgp_simulator_pkg import graphs
+from bgp_simulator_pkg import EngineTestConfig
+
+from bgp_simulator_pkg import BGPSimpleAS
+from bgp_simulator_pkg import ASNs
+from bgp_simulator_pkg import SubprefixHijack
+
+from rovpp_pkg import ROVPPAnn
+from rovpp_pkg import ROVPPV2SimpleAS
+
+
+class Config181(EngineTestConfig):
+    """Contains config options to run a test"""
+
+    name = "181"
+    desc = "Subprefix Hijack Attack with v2"
+    scenario = SubprefixHijack(attacker_asns={ASNs.ATTACKER.value},
+                               victim_asns={ASNs.VICTIM.value},
+                               AdoptASCls=ROVPPV2SimpleAS,
+                               BaseASCls=BGPSimpleAS,
+                               AnnCls=ROVPPAnn)
+    graph = graphs.Graph011()
+    non_default_as_cls_dict: Dict[int, Type[AS]] = {
+        5: ROVPPV2SimpleAS,
+        6: ROVPPV2SimpleAS,
+        1: ROVPPV2SimpleAS,
+        11: ROVPPV2SimpleAS,
+        12: ROVPPV2SimpleAS
+    }
+    propagation_rounds = 1
