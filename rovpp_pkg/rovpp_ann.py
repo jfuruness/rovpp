05763b392c567a9127821fff8ec0035569f71d22
Justin Furuness <jfuruness@gmail.com>
2023-08-31 08:05:23 -0400

    ROV++ updates

--- rovpp_pkg/rovpp_ann.py
+++ rovpp_pkg/rovpp_ann.py
@@ -15,12 +15,6 @@ class ROVPPAnn(Announcement):
     preventive: bool = False
     attacker_on_route: bool = False
 
-    # I think this might be redundant since this is in the parent class
-    # But I'm not 100% sure so just to avoid errors transitioning to BGPy
-    # I'm adding it here (Aug 17 2023)
-    def __hash__(self):
-        return hash(str(self))
-
     def __to_yaml_dict__(self):
         """This optional method is called when you call yaml.dump()
 
---
+++ scripts/inheritance_test.py
@@ -0,0 +1,51 @@
+# Doing this to test if dataclasses inherit __hash___
+from dataclasses import dataclass, asdict
+from typing import Tuple
+
+from frozendict import frozendict
+from yamlable import yaml_info
+
+from bgpy import Announcement
+
+
+@yaml_info(yaml_tag="ROVPPAnn")
+@dataclass(slots=True, frozen=True)
+class ROVPPAnn(Announcement):
+    holes: Tuple[str] = ()
+    blackhole: bool = False
+    preventive: bool = False
+    attacker_on_route: bool = False
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __to_yaml_dict__(self):
+        """This optional method is called when you call yaml.dump()
+
+        We need to overwrite this from the base class because the holes attr
+        contains a dictionary so we need to do this
+        """
+
+        dct = asdict(self)
+        return frozendict(dct)
+@yaml_info(yaml_tag="ROVPPAnn")
+@dataclass(slots=True, frozen=True)
+class ROVPPAnn2(Announcement):
+    holes: Tuple[str] = ()
+    blackhole: bool = False
+    preventive: bool = False
+    attacker_on_route: bool = False
+
+    def __to_yaml_dict__(self):
+        """This optional method is called when you call yaml.dump()
+
+        We need to overwrite this from the base class because the holes attr
+        contains a dictionary so we need to do this
+        """
+
+        dct = asdict(self)
+        return frozendict(dct)
+
+a = ROVPPAnn(prefix="1", as_path=(1,), timestamp=0, seed_asn=0, roa_valid_length=True, roa_origin=0, recv_relationship=None)
+b = ROVPPAnn(prefix="1", as_path=(1,), timestamp=0, seed_asn=0, roa_valid_length=True, roa_origin=0, recv_relationship=None)
+assert hash(a) == hash(b)
